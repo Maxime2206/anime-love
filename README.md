6d9a93e79c174d13641adae830fddac3f27aaf30
maxime <maxime.breu21@gmail.com>
2024-11-18 16:31:32 +0100

    UPDATE OF THE DAY/WEEK

--- README.md
+++ README.md
@@ -4,7 +4,19 @@
 # (Welcome to anime lovers here will be updated the TOP 5 anime per day!)
 # the color is updated compared to the animated and all animated will be added none will be excluded
 
+ SNK LAST SEASON P2 = rating: 8.6
+ Cowboy Bebop = rating: 8.5
+ Mushishi = rating: 8.2
+ Ghost in the shell = rating: 8.0
+ Hellsing Ultimate = rating: 7.6
+
 
 # Version Français :
 # (Bienvenue sur les amoureux d'anime ici seront mis a jour le TOP 5 des animé par jour !)
 # la couleur est mis a jour par rapport a l'animé et tout animé sera ajouté aucun ne sera exclu
+
+ ATTAQUE DES TITANS DERNIÈRE SAISON PARTIE 2 = évalué: 8.6
+ Cowboy Bebop = évalué: 8.5
+ Mushishi = évalué: 8.2
+ Ghost in the shell = évalué: 8.0
+ Hellsing Ultimate = évalué: 7.6
--- src/app/anime-list/anime-list.component.ts
+++ src/app/anime-list/anime-list.component.ts
@@ -8,10 +8,10 @@ import { Component } from '@angular/core';
 export class AnimeListComponent {
   // Données des animes avec les notes et une couleur de fond spécifique
   animes = [
-    { title: 'Naruto', rating: 4.5, backgroundColor: '#ff5733' },  // orange
-    { title: 'One Piece', rating: 4.8, backgroundColor: '#3498DB' },  // Bleu
-    { title: 'Attack on Titan', rating: 4.9, backgroundColor: '#873600' },  // marron
-    { title: 'Demon Slayer', rating: 5.0, backgroundColor: '#d80808' },  // rouge
-    { title: 'My Hero Academia', rating: 4.6, backgroundColor: '#239b56' },  // vert
+    { title: 'SNK LAST SEASON P2', rating: 8.6, backgroundColor: '#873600' },  // orange
+    { title: 'Cowboy Bebop', rating: 8.5, backgroundColor: '#3498DB' },  // Bleu
+    { title: 'Mushishi', rating: 8.2, backgroundColor: '#fbfcfc' },  // blanc
+    { title: 'Ghost in the shell', rating: 8.0, backgroundColor: '#979a9a' },  // gris clair
+    { title: 'Hellsing Ultimate', rating: 7.6, backgroundColor: '#e61010' },  // rouge
   ];
 }
